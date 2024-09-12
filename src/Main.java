18ae21fc4a6a5a41e8ceb217c0ead2f9b067ab21
zqlabc <2055681022@qq.com>
2024-09-12 19:14:41 +0800

    插入 选择 冒泡

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+/**
+ * ClassName: ${NAME}
+ * Package:
+ * Description:
+ *
+ * @Author 曾清林
+ * @Create ${DATE} ${TIME}
+ * @Version 1.0
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/class002/SelectBubbleInsert.java
@@ -0,0 +1,64 @@
+package class002;
+
+/**
+ * ClassName: SelectBubbleInsert
+ * Package: class002
+ * Description:
+ *
+ * @Author 曾清林
+ * @Create 2024/9/11 23:07
+ * @Version 1.0
+ */
+public class SelectBubbleInsert {
+    //交换两个数
+    public static void swap(int[] arr, int i, int j) {
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+    }
+
+    //选择排序
+    public static void selectionSort(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+        for (int minIndex, i = 0; i < arr.length - 1; i++) {
+            //找到最小值放到i位置
+            minIndex = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            swap(arr, i, minIndex);
+        }
+    }
+
+    //冒泡排序
+    public static void bubbleSort(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+        for (int end = arr.length - 1; end > 0; end--) {
+            for (int i = 0; i < end; i++) {
+                if (arr[i] > arr[i + 1]) {
+                    swap(arr, i, i + 1);
+                }
+            }
+        }
+    }
+
+    //插入排序
+    public static void insertSort(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+        for (int i = 1; i < arr.length; i++) {
+            //0~i-1有序了，新来的数是i，向左看
+            //j是当前数的前一个数；j+1是当前数
+            for (int j = i - 1; j >= 0 && arr[j] > arr[j + 1]; j--) {
+                swap(arr, j, j + 1);
+            }
+        }
+    }
+}
